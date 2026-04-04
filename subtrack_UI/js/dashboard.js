7cfadffc3b82ae2dac743a4ac6643aa1ef95ed39
Aanod <Aanod.mohamed@georgebrown.ca>
2026-04-03 20:50:03 -0400

    pulled sub and displayed sub data

---
+++ subtrack_UI/js/dashboard.js
@@ -0,0 +1,47 @@
+// stat cards
+const API_BASE = "http://subtrack.spiraml.com";
+const token = localStorage.getItem("token");
+const userId = localStorage.getItem("userId");
+// If no token, user is not logged in. Send them to login page.
+if (!token) { window.location.href = "login.html"; }
+// Get dashboard data
+fetch(API_BASE + "/api/analytics/dashboard/" + userId, {
+    headers: { "Authorization": "Bearer " + token }
+})
+.then(response => response.json())
+    .then(data => {
+        document.getElementById("burnRate").textContent = "$" +
+            data.monthlyBurnRate;
+        document.getElementById("activeCount").textContent =
+            data.activeSubscriptionCount;
+        document.getElementById("trialCount").textContent = data.freeTrialCount +
+" free trials";
+        document.getElementById("dueThisWeek").textContent =
+            data.paymentsDueThisWeek;
+        document.getElementById("savedAmount").textContent = "$" +
+            data.savedFromCancelled;
+    });
+
+// upcoming sub table
+const table = document.getElementById("subscriptionsTable");
+
+fetch(API_BASE + "/api/subscriptions/" + userId, {
+    headers: { "Authorization": "Bearer " + token }
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        table.innerHTML = "";
+
+        data.forEach(sub => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+            <td>${sub.serviceName}</td>
+            <td>$${sub.cost}</td>
+            <td>${sub.billingCycle}</td>
+            <td>${sub.category}</td>
+            <td>${sub.status}</td>
+        `;
+            table.appendChild(row);
+        });
+    });
